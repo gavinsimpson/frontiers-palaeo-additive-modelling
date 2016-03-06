94553c53eb68d2256e41de62567f73b0f6ce511b
Gavin Simpson <ucfagls@gmail.com>
2016-03-06 13:48:58 -0600

    separate fodler for functions; add a simulate.gamm method

---
+++ functions/simulate.gamm.R
@@ -0,0 +1,23 @@
+`simulate.gamm` <- function(object, nsim = 1, seed = NULL, newdata,
+                            freq = FALSE, unconditional = FALSE, ...) {
+    stopifnot(require("MASS"))
+    if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE))
+        runif(1)
+    if (is.null(seed))
+        RNGstate <- get(".Random.seed", envir = .GlobalEnv)
+    else {
+        R.seed <- get(".Random.seed", envir = .GlobalEnv)
+        set.seed(seed)
+        RNGstate <- structure(seed, kind = as.list(RNGkind()))
+        on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
+    }
+
+    if (missing(newdata)) {
+        newdata <- object$gam$model
+    }
+    Rbeta <- mvrnorm(n = nsim, coef(object$gam),
+                     vcov(object$gam, freq = freq, unconditional = unconditional))
+    Xp <- predict(object$gam, newdata = newdata, type = "lpmatrix")
+    sims <- Xp %*% t(Rbeta)
+    sims
+}
