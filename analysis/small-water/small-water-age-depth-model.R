15b48da2c42bde594111ec7d511e5fb9f94b26fb
Gavin Simpson <ucfagls@gmail.com>
2016-03-06 22:57:08 -0600

    updates to the small water example

--- analysis/small-water/small-water-age-depth-model.R
+++ analysis/small-water/small-water-age-depth-model.R
@@ -4,60 +4,11 @@
 
 ## Monotonic spline model
 
-## function to make reproducing this easier below
-`monoSpline` <- function(depth, date, error = NULL, increasing = FALSE,
-                         bs = "cr", k = 10, fx = FALSE, ...) {
-    stopifnot(require("mgcv"))
-    if(is.null(error)) {
-        error <- rep(0, length(depth))
-        W <- rep(1, length(depth))
-    } else {
-        W <- 1 / error
-        W[1] <- W[2] * error[2]
-    }
-    df <- data.frame(Depth = depth, Date = date, error = error)
-    mod <- gam(Date ~ s(Depth, k = k, bs = bs, fx = fx), data = df,
-               fit = FALSE)
-    sm <- smoothCon(s(Depth, k = k, bs = bs, fx = fx), data = df,
-                    knots = NULL)[[1]]
-    ## Fm are the constraints to enforce monotonicity
-    Fm <- mono.con(sm$xp, up = increasing)
-    G <- list(X = sm$X, C = matrix(0,0,0), sp = mod$sp, p = -sm$xp,
-              y = df$Date, Ain = Fm$A, bin = Fm$b,
-              S = sm$S, off = 0, w = W) ## w = df$Date*0+1
-    ## fitted parameters
-    p <- pcls(G)
-    ## fitted values
-    pm <- Predict.matrix(sm, data = data.frame(Depth = df$Depth))
-    fit <- pm %*% p
-    ## set up return object
-    ## this needs all info for a predict() method to work, inc
-    ## sm - smoothing matrix
-    ## p  - a vector of fitted coefficients; the object returned from pcls
-    ##
-    ## also return fitted values
-    out <- list(y = df$Date,
-                fitted.values = drop(fit),
-                coefficients = p,
-                residuals = drop(df$Date - fit),
-                sm = sm)
-    class(out) <- "monoSpline"
-    out
-}
-
-## predict S3 method
-`predict.monoSpline` <- function(object, newdata, ...) {
-    if(missing(newdata))
-        yhat <- fitted(object)
-    else {
-        yhat <- Predict.matrix(object$sm, data = newdata) %*% coef(object)
-    }
-    yhat
-}
-
 ## R Packages
 library("mgcv")                         # explicit but `monoSpline()` will check mgcv is available
 library("ggplot2")                      # for plotting
+
+source("./functions/monoSpline.R")
 
 ## SMALL1 core
 small <- read.csv("./data/small-water/small1-dating.csv")
--- analysis/small-water/small-water-analysis.R
+++ analysis/small-water/small-water-analysis.R
@@ -20,9 +20,9 @@ head(small)
 
 ## Generate a plot of the data
 plt <- ggplot(small, aes(x = Year, y = d15N)) +
-    geom_point() +
-        theme_bw() +
-            ylab(expression(delta^{15}*N ~(Bulk~Organic~Matter)))
+    geom_point(col = "darkgrey") +
+    theme_bw() +
+    ylab(expression(delta^{15}*N ~(Bulk~Organic~Matter)))
 plt
 
 ## model it using gamm()
@@ -36,15 +36,9 @@ plot(mod$gam)
 ## Predict from model
 newYear <- with(small, data.frame(Year = seq(min(Year), max(Year), length.out = 100)))
 newYear <- cbind(newYear, data.frame(predict(mod$gam, newYear, se.fit = TRUE)))
-crit <- qt(0.975, df = df.residual(mod$gam))
-newYear <- transform(newYear,
-                     upper = fit + (crit * se.fit),
-                     lower = fit - (crit * se.fit))
 
 ## Draw the fitted spline on the data
 plt.fit <- ggplot(small, aes(x = Year, y = d15N)) +
-    geom_ribbon(data = newYear, aes(y = fit, ymax = upper, ymin = lower, x = Year),
-                fill = "grey", alpha = 0.3) +
     geom_line(data = newYear, aes(y = fit, x = Year), col = "black") +
     geom_point(col = "darkgrey") +
     theme_bw() +
@@ -53,12 +47,18 @@ plt.fit
 
 ## Need to do this manually, not through `fd` - write a function
 set.seed(1)
-nsim <- 50
-randSims <- simulate(mod, nsim = nsim, newdata = newYear, unconditional = TRUE)
-colnames(randSims) <- paste0("sim", seq_len(nsim))
+nsim <- 10000
+take <- 50
+sims <- simulate(mod, nsim = nsim, newdata = newYear, unconditional = TRUE)
+randSims <- sims[, sample(nsim, take)]
+colnames(randSims) <- paste0("sim", seq_len(take))
 randSims <- setNames(stack(as.data.frame(randSims)), c("simulated", "run"))
-randSims <- transform(randSims, Year = rep(newYear$Year, nsim),
+randSims <- transform(randSims, Year = rep(newYear$Year, take),
                       simulated = simulated)
+
+## Add a CI to the fitted spline
+CI <- apply(sims, 1L, quantile, probs = c(0.025, 0.975))
+newYear <- transform(newYear, lower = CI[1,], upper = CI[2,])
 
 plt.sim <- ggplot(newYear, aes(x = Year, y = fit)) +
     geom_line(data = randSims, mapping = aes(y = simulated, x = Year, group = run),
@@ -101,7 +101,14 @@ newYear <- transform(newYear,
                      YearIncr = sigDYear$incr,
                      YearDecr = sigDYear$decr)
 
-plt.fit2 <- plt.fit +
+plt.fit2 <- ggplot(small, aes(x = Year, y = d15N)) +
+    geom_ribbon(data = newYear,
+                mapping = aes(ymax = upper, ymin = lower, y = fit, x = Year),
+                fill = "grey", alpha = 0.3) +
+    geom_line(data = newYear, aes(y = fit, x = Year), col = "black") +
+    geom_point(col = "darkgrey") +
+    theme_bw() +
+    ylab(expression(delta^{15}*N ~(Bulk~Organic~Matter))) +
     geom_line(data = newYear, mapping = aes(y = YearIncr), size = 1.5) +
     geom_line(data = newYear, mapping = aes(y = YearDecr), size = 1.5)
 plt.fit2
@@ -116,8 +123,8 @@ plt.fit2
 ## grid.draw(displayPlt)
 ## dev.off()
 
-combinedPlt <- plot_grid(plt.fit, plt.sim, derivPlt, plt.fit2, labels = "AUTO",
+combinedPlt <- plot_grid(plt, plt.sim, derivPlt, plt.fit2, labels = "AUTO",
                          align = "vh")
 combinedPlt
 
-ggsave("./figures/small-water-display.pdf", combinedPlt, width = 10, height = 9)
+ggsave("./figures/small-water-display.pdf", combinedPlt, width = 10, height = 7)
